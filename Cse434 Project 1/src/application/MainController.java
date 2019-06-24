f81d3192b49ad320ccefdf7027d00f0b62eccb93
Barun Roy <roy.barun@northsouth.edu>
2019-06-24 22:57:53 +0600

    Wrote some initial Html and CSS codes in the Resume Generator.html file

--- Cse434 Project 1/src/application/MainController.java
+++ Cse434 Project 1/src/application/MainController.java
@@ -52,42 +52,7 @@ public class MainController implements EventHandler<ActionEvent> {
 			
 			try {
 				PrintWriter outputStream = new PrintWriter(filename);
-				outputStream.println("<!Doctype html>\r\n" + 
-						"\r\n" + 
-						"<html>\r\n" + 
-						"\r\n" + 
-						"<head>\r\n" + 
-						"\r\n" + 
-						"	                    <link href=\\\"https://fonts.googleapis.com/css?family=Nunito:200,600\\\" rel=\\\"stylesheet\\\">\r\n" + 
-						"					    <link rel=\\\"stylesheet\\\" href=\\\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\\\"> \r\n" + 
-						"					    <script src=\\\"https://code.jquery.com/jquery-3.3.1.slim.min.js\\\"></script> \r\n" + 
-						"					    <script src=\\\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\\\"></script>\r\n" + 
-						"					    <script src=\\\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\\\"></script>\r\n" + 
-						"					    <link rel=\\\"stylesheet\\\" href=\\\"{{asset('css/all.css')}}\\\">\r\n" + 
-						"\r\n" + 
-						"\r\n" + 
-						"	<title>Resume Generator</title>\r\n" + 
-						"\r\n" + 
-						"</head>\r\n" + 
-						"\r\n" + 
-						"<body>\r\n" + 
-						"\r\n" + 
-						"	<div class=\"container\">\r\n" + 
-						"		\r\n" + 
-						"\r\n" + 
-						"			<h2 id=\"name\">Name: "+na+"</h2>\r\n" + 
-						"			<h3 id=\"address\">Address: "+ad+" </h3>\r\n" + 
-						"			<h3 id=\"mobile\">Mobile: "+mo+"</h3>\r\n" + 
-						"			<h3 id=\"email\">Email: "+em+"</h4>\r\n" + 
-						"			<h3 id=\"dateofbirth\">Date of Birth: "+da+"</h5>\r\n" + 
-						"		\r\n" + 
-						"\r\n" + 
-						"	</div>\r\n" + 
-						"\r\n" + 
-						"\r\n" + 
-						"</body>\r\n" + 
-						"</html>\r\n"+""
-						);
+				outputStream.println("");
 				
 				outputStream.close();
 				System.out.println("Generated");
