ec6461215e7b5a8d0cf062b0bcef2a52760fd407
Barun Roy <roy.barun@northsouth.edu>
2019-07-07 12:22:03 +0600

    Added the remaining html and css codes in the MainController file

--- Cse434 Project 1/src/application/MainController.java
+++ Cse434 Project 1/src/application/MainController.java
@@ -228,6 +228,141 @@ public class MainController implements EventHandler<ActionEvent> {
 						"            </ul>\r\n" + 
 						"          </div>\r\n" + 
 						"\r\n" + 
+						"\r\n" + 
+						"          <div class=\"lists\">\r\n" + 
+						"            <h2>Personal Skills</h2>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Ambitious and Hard working</li>\r\n" + 
+						"              <li>Reliable and Professional</li>\r\n" + 
+						"              <li>Positive thinker</li>\r\n" + 
+						"              <li>Team player</li>\r\n" + 
+						"              <li>Fast learner</li>\r\n" + 
+						"              <li>Good Communication skills</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"          <div class=\"lists\">\r\n" + 
+						"            <h2>Professional Skills</h2>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>SEO and Google analytics</li>\r\n" + 
+						"              <li>social media strategy</li>\r\n" + 
+						"              <li>marketing</li>\r\n" + 
+						"              <li>web content developer</li>\r\n" + 
+						"              <li>corporate blogging</li>\r\n" + 
+						"              <li>photo editing</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"          <div class=\"lists\">\r\n" + 
+						"            <h2>Personal Assessment</h2>\r\n" + 
+						"            <p>\r\n" + 
+						"              I am very comfortable at working both in a group or individually and like to work in competitive environment with huge mental strength and good physic.\r\n" + 
+						"            <p>\r\n" + 
+						"          </div>\r\n" + 
+						"        </div> <!-- end wrapper -->\r\n" + 
+						"      </div> <!-- end section left -->\r\n" + 
+						"\r\n" + 
+						"             \r\n" + 
+						"      \r\n" + 
+						"      <div class=\"section-right\">\r\n" + 
+						"        <div class=\"wrapper\">\r\n" + 
+						"          <div class=\"content\">\r\n" + 
+						"            <h2>Career Objective</h2>\r\n" + 
+						"            <p>To work in a pragmatic way in an organization where I can show my talent and enhance my skills to meet company goals and objectives with full integrity and passion.</p>\r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"          <div class=\"content\">\r\n" + 
+						"            <h2>Educatinal Qualification</h2>\r\n" + 
+						"\r\n" + 
+						"            <h3>Graduation Information</h3>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Major:</li>\r\n" + 
+						"              <li>Institution:</li>\r\n" + 
+						"              <li>Year of Graduation:</li>\r\n" + 
+						"              <li>CGPA: on a scale of 4</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"            <h3>A'Level/HSC Information</h3>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Institution:</li>\r\n" + 
+						"              <li>Year of Graduation:</li>\r\n" + 
+						"              <li>CGPA: on a scale of 5</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"            <h3>O'Level/SSC Information</h3>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Institution:</li>\r\n" + 
+						"              <li>Year of Graduation:</li>\r\n" + 
+						"              <li>CGPA: on a scale of 5</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"          <div class=\"content\">\r\n" + 
+						"            <h2>work experience</h2>\r\n" + 
+						"\r\n" + 
+						"            <h3>social media producer</h3>\r\n" + 
+						"            <h4>Red Media Company | Feb 2013 - Dec 2014</h4>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Managed social media accounts</li>\r\n" + 
+						"              <li>Created social media graphics for daily postings</li>\r\n" + 
+						"              <li>Increased social media following and clicks by 200%</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"\r\n" + 
+						"            <h3>communications intern</h3>\r\n" + 
+						"            <h4>Red Media Interactive | Apr 2012 - Jan 2013</h4>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Developed a social media strategy to increase the following of the company website</li>\r\n" + 
+						"              <li>Spearheaded a viral campaign for one of the company's new products</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"          <div class=\"content\">\r\n" + 
+						"            <h2>Achievements</h2>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Successful participant of Independence Day Quiz Competition 2009 by YWSS</li>\r\n" + 
+						"              <li>Received 25% scholarship for 12 semesters from NSU</li>\r\n" + 
+						"            </ul>   \r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"          <div class=\"content\">\r\n" + 
+						"            <h2>Co-Extracurricular Activities</h2>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Executive body member of MLIS Community Service Club</li>\r\n" + 
+						"              <li>Senior member of MLIS Drama Club</li>\r\n" + 
+						"              <li>Sub-Executive body member of NSU Computer and Engineering Club</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"          </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"          <div class=\"content\">\r\n" + 
+						"            <h2>Computer Expertise</h2>\r\n" + 
+						"            <ul>\r\n" + 
+						"              <li>Very good Knowledge over MS Office (MS Word, MS Excel, MS PowerPoint)</li>\r\n" + 
+						"              <li>Good knowledge in Java, MySQL Database, HTML and CSS</li>\r\n" + 
+						"              <li>Sound knowledge in Android Studio</li>\r\n" + 
+						"            </ul>\r\n" + 
+						"          </div>\r\n" + 
+						"        </div>\r\n" + 
+						"      </div> <!-- end section right -->\r\n" + 
+						"    </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"    <div class=\"footer\">\r\n" + 
+						"      <p class=\"ps\">Copyright &copy; 2019, Barun Roy Resume</p>\r\n" + 
+						"    </div>\r\n" + 
+						"\r\n" + 
+						"\r\n" + 
+						"<div style=\"margin-top:500px;\"></div>\r\n" + 
+						"  \r\n" + 
+						"</body>\r\n" + 
+						"</html>\r\n" + 
+						"\r\n" + 
 						"");
 				
 				outputStream.close();
