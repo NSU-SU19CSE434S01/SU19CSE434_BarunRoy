184e050de171f14587ec81cc4b025103dc3fe40f
Barun Roy <roy.barun@northsouth.edu>
2019-07-10 13:52:36 +0600

    Wrote some codes for O Level/SSC Information in the MainController and added the fxid for the TextFields

--- Cse434 Project 1/src/application/MainController.java
+++ Cse434 Project 1/src/application/MainController.java
@@ -77,14 +77,24 @@ public class MainController implements EventHandler<ActionEvent> {
 	public TextField cgpa2;
 	
 	
-
+	/*O'Level/SSC Information*/
+	
+	@FXML
+	public TextField olevelorssc;
+	@FXML
+	public TextField institution3;
+	@FXML
+	public TextField yearofgraduation3;
+	@FXML
+	public TextField cgpa3;
+	
 	
 	
 	@FXML
 	public Button Generate;
 	
 	@FXML
-	String na,ad,mo,em,da,ca,pe1,pe2,pe3,pe4,pe5,pe6,pr1,pr2,pr3,pr4,pr5,pr6,ma,in1,ye1,cg1,al,in2,ye2,cg2;
+	String na,ad,mo,em,da,ca,pe1,pe2,pe3,pe4,pe5,pe6,pr1,pr2,pr3,pr4,pr5,pr6,ma,in1,ye1,cg1,al,in2,ye2,cg2,ol,in3,ye3,cg3;
 	
 	public void press(ActionEvent args) {
 		Generate.setOnAction(this);
