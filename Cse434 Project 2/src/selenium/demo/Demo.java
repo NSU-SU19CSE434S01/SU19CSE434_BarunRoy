e3f8bfc09159d55ecb938a6f3770b2aca960dd8c
Barun Roy <roy.barun@northsouth.edu>
2019-08-09 23:58:16 +0600

    Wrote a test case to check if the test case runs in smoothly or not

--- Cse434 Project 2/src/selenium/demo/Demo.java
+++ Cse434 Project 2/src/selenium/demo/Demo.java
@@ -1,12 +1,35 @@
 package selenium.demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws InterruptedException {
 		
+		System.setProperty("webdriver.chrome.driver","C:\\Selinium 2\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.gmail.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("roybarun36@gmail.com");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span")).click();
+		Thread.sleep(2000);
+		String at = driver.getTitle();
+		String et = "gmail";
+		driver.close();
+		if(at.equalsIgnoreCase(et))
+		{
+			System.out.println("Test Successful");  
+		}
+		
+		else {
+			
+			System.out.println("Test Failure");
+		}
+
 	}
-		
-		
 
 	
 
