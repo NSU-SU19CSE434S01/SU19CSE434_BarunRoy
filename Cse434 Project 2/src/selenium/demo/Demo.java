26942764668e84ef4c626356d59ae6e23aed9d2d
Barun Roy <roy.barun@northsouth.edu>
2019-08-10 11:49:04 +0600

    I selected website number 5 that is automationpractice with the title My Store.Wrote the code that automatically opens the webpage and gets the title

--- Cse434 Project 2/src/selenium/demo/Demo.java
+++ Cse434 Project 2/src/selenium/demo/Demo.java
@@ -9,25 +9,15 @@ public class Demo {
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Selinium 2\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.gmail.com/");
+		WebDriver driver = new ChromeDriver();             
+		driver.get("http://www.automationpractice.com/");
 		driver.manage().window().maximize();
-		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("roybarun36@gmail.com");
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
 		Thread.sleep(2000);
-		String at = driver.getTitle();
-		String et = "gmail";
 		driver.close();
-		if(at.equalsIgnoreCase(et))
-		{
-			System.out.println("Test Successful");  
-		}
 		
-		else {
-			
-			System.out.println("Test Failure");
-		}
 
 	}
 
