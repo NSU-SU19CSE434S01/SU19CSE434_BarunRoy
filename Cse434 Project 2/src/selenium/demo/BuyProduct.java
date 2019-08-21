c0551b6e43d6c6f3f20634491e47b62f184e6f90
Barun Roy <roy.barun@northsouth.edu>
2019-08-21 11:16:29 +0600

    Added some more codes in the class buyproduct

--- Cse434 Project 2/src/selenium/demo/BuyProduct.java
+++ Cse434 Project 2/src/selenium/demo/BuyProduct.java
@@ -2,12 +2,14 @@ package selenium.demo;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class BuyProduct {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Selinium 2\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -20,6 +22,22 @@ public class BuyProduct {
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
+		
+        Actions action = new Actions(driver);
+		
+		action.moveToElement(driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a"))).build().perform();
+		
+		Thread.sleep(1000);
+		
+		driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/ul/li[3]/a")).click();
+		
+		Thread.sleep(1000);
+		
+		driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/div[2]/a[1]/span")).click();
+		
+		Thread.sleep(5000);
+		
+		driver.close();
 
 	}
 
